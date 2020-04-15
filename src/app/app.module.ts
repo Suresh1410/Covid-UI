7a21003c201c669c8576a5cee7c33124825e6fc4
Suresh1410 <sureshkasinathan96@gmail.com>
2020-04-15 17:36:55 +0530

    added post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
+import { PostRequestComponent } from './post-request/post-request.component';
+import {  ReactiveFormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponentComponent
+    LoginComponentComponent,
+    PostRequestComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/post-request/post-request.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { NgbDatepicker } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-post-request',
+  templateUrl: './post-request.component.html',
+  styleUrls: ['./post-request.component.css']
+})
+export class PostRequestComponent implements OnInit {
+
+  postForm: FormGroup;
+  @ViewChild('requestDate', { static: false }) requestDate: NgbDatepicker;
+  @ViewChild('requestEndDate', { static: false }) requestEndDate: NgbDatepicker;
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(){
+    this.postForm = this.formBuilder.group({
+      
+      type: [""],
+      subType:[""],
+      quantity: [""],
+      requestDate: [""],
+      requestEndDate:[""],
+      address1: [""],
+      address2: [""],
+      address3: [""],
+      city: [""],
+      state: [""],
+      country:[""],
+      pinCode: [""],
+      phone: [""],
+      email: [""]
+    });
+  }
+
+  onSubmit(){
+    console.log(this.postForm.getRawValue());
+  }
+
+  dateFormatter(event) {
+    if (event.target.value.length === 4 || event.target.value.length === 7) {
+        if (event.keycode !== 8) {
+            event.target.value += "-";
+        }
+    }
+}
+
+restrictAlphabets(event) {
+  let pattern = /^[0-9]+$/;
+  return pattern.test(event.key);
+}
+
+}
